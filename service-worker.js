a10312d50a280bff6b3cab6ffc3751afc7cd264d
0x-dada <464720742@qq.com>
2026-09-20 19:41:59 +0800

    SW v5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cryptojournal-v3';
+const CACHE_NAME = 'cryptojournal-v5';
 const ASSETS = ['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png','/icons/apple-touch-icon.png','/favicon.svg'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))); });
